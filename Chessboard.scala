802ad7e814f3ba4831d127cb750873560477e3c1
jerrybear16 <jeroenwolfe@gmail.com>
2020-12-09 12:44:59 -0500

    Preliminary ideas for chess app
    created my Chessboard object and put in notes about how I want to implement the chess board and the pieces

---
+++ Chessboard.scala
@@ -0,0 +1,16 @@
+
+
+
+object Chessboard{
+
+    
+    //2D array represents the board
+    //keeps track of all the pieces
+    //getPos(Piece) for finding where a piece is
+    //setPos(Piece) for moving a piece
+    //ArrayList or something for storing the pieces
+        //one list for white, one list for black
+    //
+
+
+}
