70872fa7adcf236e643dfaff41846c34b4874c6a
jerrybear16 <jeroenwolfe@gmail.com>
2020-12-11 16:16:07 -0500

    nothing useful to say here

--- practice.scala
+++ practice.scala
@@ -1,5 +1,6 @@
 
-
+import scala.io.Source._
+import scala.util.Random
 
 
 object practice extends App{
@@ -10,10 +11,19 @@ object practice extends App{
     val anotherList = List(3,4)
 
     for(i <- myList ; j <- anotherList){
+
         if(i%2 == 0) println(i+" Yes")
         else println(i+" no")
         if(j%2 == 0) println(j+" Even")
         else println(j+" odd")
 
     }
+
+    println(toInt("124"))
+
+    def toInt(s: String): Option[Int] = 
+    { try { Some(s.toInt) }
+    catch { case e: Exception => None } }
+
+    
 }
