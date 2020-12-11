dd38c3c6f0e520a4e58c55afcb3933211b66dfae
jerrybear16 <jeroenwolfe@gmail.com>
2020-12-11 15:23:42 -0500

    messing with for loops

--- practice.scala
+++ practice.scala
@@ -2,7 +2,18 @@
 
 
 
-class practice extends App{
+object practice extends App{
 
     println("class extends app")
+
+    val myList = List(1,2,5)
+    val anotherList = List(3,4)
+
+    for(i <- myList ; j <- anotherList){
+        if(i%2 == 0) println(i+" Yes")
+        else println(i+" no")
+        if(j%2 == 0) println(j+" Even")
+        else println(j+" odd")
+
+    }
 }
