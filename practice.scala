f1f00a998cc7c4df0668a03639c4af783d22fdf2
jerrybear16 <jeroenwolfe@gmail.com>
2020-12-11 14:43:55 -0500

    restarting repo

--- practice.scala
+++ practice.scala
@@ -2,18 +2,7 @@
 
 
 
+class practice extends App{
 
-object practice extends App {
-
-   //println("hello")
-
-   var a:String ="hello"
-   val b ="goodbye"
-
-   a=b
-
-   println(a)
-   println(b)
-
-
+    println("class extends app")
 }
